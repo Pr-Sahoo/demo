f205b9bf8cc8a3edb44f896f65d3007c10e785f0
Pr-Sahoo <pranayaranjan4@gmail.com>
2024-10-14 21:26:17 +0530

    adding new change

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
+import React, {useState} from 'react';
 function App() {
+  const [mode, setMode] = useState("dark");
+  const toogle = () => {
+    if(mode === "light"){
+      setMode("dark");
+    }else{setMode("light")};
+  }
   return (
     <>
       {/* <Navbar title= "Homelander" about="Homelander"/> */}
-      <Navbar title="Icoder" about="blog"/>
+      <Navbar title="Icoder" about="blog" toogle={toogle} mode={mode}/>
       <div className="container my-4">
       <TextForm heading="Enter text to analyze"/>
       {/* <About/> */}
--- components/TextForm.js
+++ components/TextForm.js
@@ -28,6 +28,12 @@ export default function TextForm(props) {
     const remove = () => {
         setText(text.trim().replace(/\s+/g, " "));
     }
+    // const theme = () => {
+    //     document.body.style.backgroundColor = "black";
+    // }
+    // const theme2 = () => {
+    //     document.body.style.backgroundColor = "white";
+    // }
     const [text, setText] = useState("Enter your text here");
     // text = "Homelander"; // wrong way to set text
     // setText("Homelander is great");
@@ -44,6 +50,8 @@ export default function TextForm(props) {
             <button className="btn btn-warning mx-2" onClick={toClear}>Clear</button>
             <button className="btn btn-secondary mx-2" onClick={copyText}>copy text</button>
             <button className="btn btn-info mx-2" onClick={remove}>Remove space</button>
+            {/* <button className='btn btn-success mx-2' onClick={theme}>change theme</button>
+            <button className='btn btn-warning mx-2' onClick={theme2}>white</button> */}
         </div>
         <div className="container my-2">
             <h2>your text here</h2>
